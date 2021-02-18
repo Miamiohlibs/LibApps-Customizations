56dfd639ff3aaab19fa1c73e881ff2544d88aa48
Ken Irwin <irwinkr@miamioh.edu>
2021-02-18 11:51:53 -0500

    updated email-link-widget, old code in comments

--- email-link-widget.js
+++ email-link-widget.js
@@ -1,6 +1,23 @@
+/*
+This is the stand-alone version of the email-link-widget. To use it, place this code (and update the first line!) in the "Manage Account > Profile Box > Other Widgets" section linked from the gear-icon at:
+https://muohio.libapps.com/libapps/accounts.php
+
+Advantages: 
+ * doesn't rely on external code (except jQuery itself) to run.
+
+Disadvantages: 
+  * if we ever need to update the code, we'll have to update each instance individually.
+
+The "profileId" corresonds to the profile number (not the user account number). 
+You can find this number by browser-inspecting the profile image. You'll see it enclosed in a div like this: 
+<div id="s-lib-profile-image-1234" class="s-lib-profile-div s-lib-profile-center s-lib-profile-image">...</div>
+The "1234" number should be swapped into the first line of the function. 
+*/
+
 $(() => {
-  let userId = '5546';
-  let thisProfile = $('#s-lib-profile-' + userId);
+  // This function adds the email address in a visible form at the end of the profile box contact section
+  let profileId = '1234';
+  let thisProfile = $('#s-lib-profile-' + profileId);
   let emailAddress = $(thisProfile)
     .find('.s-lib-profile-email a')
     .attr('title');
